0f6548c1fd900b8ff11230e9acc31a754f1b24b8
DonciuDan <143322743+DonciuDan@users.noreply.github.com>
2024-02-26 23:57:55 +0200

    update gym

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AddEditItemComponent} from "./dashboard/add-edit-item/add-edit-item.component";
+import {AuthComponent} from "./auth/auth.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //un selector reprezinta modul de apelare al componentei (este un tag de html)
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DashboardComponent, AddEditItemComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'onlineshop-myversion';
+  title = 'onlineshop';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
+import {HomeComponent} from "./home/home.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {SuplimenteComponent} from "./suplimente/suplimente.component";
+import {SalaComponent} from "./sala/sala.component";
+import {NutritieComponent} from "./nutritie/nutritie.component";
+import {EchipamentSportivComponent} from "./echipament-sportiv/echipament-sportiv.component";
+import {AntrenamenteComponent} from "./antrenamente/antrenamente.component";
+import {TryComponent} from "./try/try.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path:"auth",component:AuthComponent,
+  },
+  {
+    path:"home",component:HomeComponent,
+  },
+  {
+    path:"dashboard",component:DashboardComponent,
+  },
+  {
+    path:"",redirectTo:"welcome",pathMatch:"full"
+  },
+  {
+    path:"welcome",component: WelcomeComponent,
+  },
+  {
+    path:"suplimente",component: SuplimenteComponent,
+  },
+  {
+    path:"sala",component: SalaComponent,
+  },
+  {
+    path:"nutritie",component: NutritieComponent,
+  },
+  {
+    path:"echipament-sportiv",component: EchipamentSportivComponent,
+  },
+  {
+    path:"antrenamente",component: AntrenamenteComponent,
+  },
+  {
+    path:"try",component: TryComponent,
+  },
+];
